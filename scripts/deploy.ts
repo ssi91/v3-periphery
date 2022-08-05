e9832381e5f62a2f8c0334ff6e0bb3b848e69095
ssi91 <sergeev.s.i1991@gmail.com>
2022-08-05 21:39:14 +0400

    add a helper to deploy contracts from hh's artifact

---
+++ scripts/deploy.ts
@@ -0,0 +1,37 @@
+import {Contract} from "typechain";
+import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/src/signers";
+import fs from "fs";
+
+const hre = require('hardhat')
+
+let initialConfig = {
+    factory: {
+        artifactPath: "",
+    },
+    weth9: {
+        artifactPath: "",
+    }
+}
+
+async function deployContractFromArtifact(signer: SignerWithAddress, artifactPath: string): Promise<Contract> {
+    let getArtifact = (artifactPath: string): { abi: object, bytecode: object } => {
+        try {
+            let data = fs.readFileSync(artifactPath)
+            return JSON.parse(data.toString())
+        } catch (e) {
+            throw "Wrong Source Path: " + e.message + " " + artifactPath
+        }
+    }
+
+    const artifact = getArtifact(artifactPath)
+    return await hre.waffle.deployContract(signer, {
+        abi: artifact.abi,
+        bytecode: artifact.bytecode
+    })
+}
+
+async function main() {
+
+}
+
+main().then(() => process.exit(0))
