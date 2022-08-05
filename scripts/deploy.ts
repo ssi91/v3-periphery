e7e00a3ad30a7d5077cb1978e50905f6ba8e1810
ssi91 <sergeev.s.i1991@gmail.com>
2022-08-05 23:33:51 +0400

    deploy nft position manager

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -11,7 +11,8 @@ let initialConfig = {
     },
     weth9: {
         artifactPath: "",
-    }
+    },
+    nativeTokenSymbol: "",
 }
 
 async function deployContractFromArtifact(signer: SignerWithAddress, artifactPath: string): Promise<Contract> {
@@ -37,20 +38,44 @@ async function main() {
     // WETH9
     const weth9 = await deployContractFromArtifact(signer, initialConfig.weth9.artifactPath)
     await weth9.deployed()
-    console.log(weth9.address)
+    console.log("WETH9", weth9.address)
 
     // Factory Contract
     const factory = await deployContractFromArtifact(signer, initialConfig.factory.artifactPath)
     await factory.deployed()
-    console.log(factory.address)
+    console.log("Factory", factory.address)
 
     // Router
     const RouterFactory = await ethers.getContractFactory("SwapRouter")
     const router = await RouterFactory.deploy(factory.address, weth9.address)
     await router.deployed()
-    console.log(router.address)
+    console.log("Router", router.address)
 
-    // TODO: Create NFT position manager
+    const NFTDescFactory = await ethers.getContractFactory("NFTDescriptor")
+    const nftDesc = await NFTDescFactory.deploy()
+    await nftDesc.deployed()
+    console.log("NFT Descriptor", nftDesc.address)
+
+    const PositionManagerDescFactory = await ethers.getContractFactory("NonfungibleTokenPositionDescriptor", {
+        libraries: {
+            NFTDescriptor: nftDesc.address
+        }
+    })
+    const positionManagerDescriptor = await PositionManagerDescFactory.deploy(
+        weth9.address,
+        ethers.utils.formatBytes32String(initialConfig.nativeTokenSymbol)
+    )
+    console.log("Position Manager Descriptor: ", positionManagerDescriptor.address)
+
+    // NFT Position manager
+    const PositionManagerFactory = await ethers.getContractFactory("NonfungiblePositionManager")
+    const positionManager = await PositionManagerFactory.deploy(
+        factory.address,
+        weth9.address,
+        positionManagerDescriptor.address
+    )
+    await positionManager.deployed()
+    console.log("Position Manager", positionManager.address)
 }
 
 main().then(() => process.exit(0))
